b23925629443bd69c14c7674e7cbdf28d177eb8e
Ayaz43 <65758480+Ayaz43@users.noreply.github.com>
2020-07-28 20:19:11 +0530

    Making the function app testable
    Split the code into small methods

--- fappdeploy/httptrigger/GridEventHandler.cs
+++ fappdeploy/httptrigger/GridEventHandler.cs
@@ -1,20 +1,20 @@
 using System;
-using System.Text;
-using System.Net.Http;
-using System.Net.Http.Headers;
-
+using System.IO;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
 
 public static class GridEventHandler
 {
 
-    private static string ParseEventGridValidationCode(dynamic requestObject)
+    public static string ParseEventGridValidationCode(dynamic requestObject)
     {
         var webhook_res = string.Empty;
         if (requestObject != null && requestObject[0]["data"] != null)
@@ -27,6 +27,97 @@ public static class GridEventHandler
         }
         return webhook_res;
     }
+
+    public static string getEventSource(dynamic current_event)
+    {
+        string event_source = string.Empty;
+        string[] event_data = current_event.Split(".");
+        if (event_data.Length > 1)
+        {
+            event_source = event_data[1];
+        }
+        return event_source;
+    }
+
+    public static string getEventType(dynamic current_event)
+    {
+        string event_type = string.Empty;
+        string[] event_data = current_event.Split(".");
+        event_type = getEventSource(current_event).ToLower();
+
+        for (int index = 2; index < event_data.Length; index++)
+        {
+            event_type += "-" + event_data[index].ToLower();
+        }
+
+        return event_type;
+    }
+
+    public static dynamic getRequestDataFromRequestObject(string event_source, dynamic requestObject)
+    {
+        var req_data = requestObject[0]["data"];
+
+        switch (event_source)
+        {
+            case "AppConfiguration":
+                req_data = ParseAppConfigurationEvent(requestObject);
+                break;
+
+            case "Web/sites":
+                req_data = ParseAppServiceEvent(requestObject);
+                break;
+
+            case "Storage":
+                req_data = ParseBlobStorageEvent(requestObject);
+                break;
+
+            case "ContainerRegistry":
+                req_data = ParseContainerRegistryEvent(requestObject);
+                break;
+
+            case "EventHub":
+                req_data = ParseEventHubEvent(requestObject);
+                break;
+
+            case "Devices":
+                req_data = ParseIoTDevicesEvent(requestObject);
+                break;
+
+            case "KeyVault":
+                req_data = ParseKeyVaultEvent(requestObject);
+                break;
+
+            case "MachineLearningServices":
+                req_data = ParseMachineLearningEvent(requestObject);
+                break;
+
+            case "Maps":
+                req_data = ParseMapsEvent(requestObject);
+                break;
+
+            case "Media":
+                req_data = ParseMediaEvent(requestObject);
+                break;
+
+            case "Resources":
+                req_data = ParseResourcesEvent(requestObject);
+                break;
+
+            case "ServiceBus":
+                req_data = ParseServiceBusEvent(requestObject);
+                break;
+
+            case "SignalRService":
+                req_data = ParseSignalRServiceEvent(requestObject);
+                break;
+
+            default:
+                return (ActionResult)new OkObjectResult("Unrecognized event, could not be sent");
+
+        }
+        return req_data;
+    }
+
 
     private static dynamic ParseAppConfigurationEvent(dynamic requestObject)
     {
@@ -82,14 +173,15 @@ public static class GridEventHandler
     }
 
     [FunctionName("generic_triggers")]
-    public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req, ILogger log, ExecutionContext context)
+    public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, ILogger log, ExecutionContext context)
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
 
-        string requestBody = await req.Content.ReadAsStringAsync();
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         dynamic requestObject = JsonConvert.DeserializeObject(requestBody);
         var current_event = requestObject[0]["eventType"].ToString();
 
+        //acknowledge if this is a subscription event
         if (current_event == "Microsoft.EventGrid.SubscriptionValidationEvent")
         {
             string webhook_res = ParseEventGridValidationCode(requestObject);
@@ -98,90 +190,25 @@ public static class GridEventHandler
                 return (ActionResult)new OkObjectResult($"{webhook_res}");
             }
         }
+        
+        string event_type = getEventType(current_event);
+        string event_source = getEventSource(current_event);
+       
+        string repo_name = string.Empty;
 
-        string[] event_data = current_event.Split(".");
-        string event_source = string.Empty;
-        string event_type = string.Empty;
-
-        if (event_data.Length > 1)
-        {
-            event_source = event_data[1];
+        if (!req.GetQueryParameterDictionary().TryGetValue("repoName", out repo_name)){    
+           //repo name not provided event could not be sent
+           return (ActionResult)new OkObjectResult("Github repository name not provided, could not be sent");
         }
 
-        var queryParams = System.Web.HttpUtility.ParseQueryString(req.RequestUri.Query);
-        string repo_name = queryParams.Get("repoName");
         log.LogInformation("fetching repo name from query parameters." + repo_name);
 
-        if (event_data.Length > 2 && !string.IsNullOrEmpty(repo_name))
+
+        if (!string.IsNullOrEmpty(repo_name))
         {
-            event_type = event_source.ToLower()  ;
+            var req_data = getRequestDataFromRequestObject(event_source, requestObject);
 
-            for (int index = 2; index < event_data.Length; index++)
-            {
-                event_type += "-" + event_data[index].ToLower();
-            }
-            var req_data = requestObject[0]["data"];
-
-            switch (event_source)
-            {
-                case "AppConfiguration":
-                    req_data = ParseAppConfigurationEvent(requestObject);
-                    break;
-
-                case "Web/sites":
-                    req_data = ParseAppServiceEvent(requestObject);
-                    break;
-
-                case "Storage":
-                    req_data = ParseBlobStorageEvent(requestObject);
-                    break;
-
-                case "ContainerRegistry":
-                    req_data = ParseContainerRegistryEvent(requestObject);
-                    break;
-
-                case "EventHub":
-                    req_data = ParseEventHubEvent(requestObject);
-                    break;
-
-                case "Devices":
-                    req_data = ParseIoTDevicesEvent(requestObject);
-                    break;
-
-                case "KeyVault":
-                    req_data = ParseKeyVaultEvent(requestObject);
-                    break;
-
-                case "MachineLearningServices":
-                    req_data = ParseMachineLearningEvent(requestObject);
-                    break;
-
-                case "Maps":
-                    req_data = ParseMapsEvent(requestObject);
-                    break;
-
-                case "Media":
-                    req_data = ParseMediaEvent(requestObject);
-                    break;
-
-                case "Resources":
-                    req_data = ParseResourcesEvent(requestObject);
-                    break;
-
-                case "ServiceBus":
-                    req_data = ParseServiceBusEvent(requestObject);
-                    break;
-
-                case "SignalRService":
-                    req_data = ParseSignalRServiceEvent(requestObject);
-                    break;
-
-                default:
-                    return (ActionResult)new OkObjectResult("Unrecognized event, could not be sent");
-                   
-            }
-
-            using (var httpClient = new HttpClient())
+            using (var httpClient = new System.Net.Http.HttpClient())
             {
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 httpClient.DefaultRequestHeaders.Add("User-Agent", "Awesome-Octocat-App");
